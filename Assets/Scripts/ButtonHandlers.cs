0a9bdee762bfd6f6fff1e6c0fb26ee3d6355484a
Jamie R <36210321+Lights-and-Shadows@users.noreply.github.com>
2020-09-26 12:16:46 -0400

    Imported Lab3 package, removed Kyle from Content parenting

---
+++ Assets/Scripts/ButtonHandlers.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ButtonHandlers : MonoBehaviour
+{
+
+    public Button toggle;
+
+    public GameObject session;
+
+    public TextMeshProUGUI toggleText;
+
+    public bool isPlaneFinderActive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPlaneFinderActive = true;
+
+        toggle.onClick.AddListener(TogglePlaneFinder);
+    }
+
+    public void TogglePlaneFinder()
+    {
+        if (isPlaneFinderActive)
+            isPlaneFinderActive = false;
+        else
+            isPlaneFinderActive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlaneFinderActive)
+        {
+            session.GetComponent<MakeAppearOnPlane>().enabled = true;
+            toggleText.text = "Plane Finder On";
+        }
+        else
+        {
+            session.GetComponent<MakeAppearOnPlane>().enabled = false;
+            toggleText.text = "Plane Finder Off";
+        }
+    }
+}
---
+++ Assets/Scripts/SliderHandlers.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderHandlers : MonoBehaviour
+{
+
+    public Slider rotationSlider, scaleSlider;
+
+    public Transform content;
+
+    // Update is called once per frame
+    void Update()
+    {
+        content.rotation = Quaternion.Euler(0f, rotationSlider.value, 0f);
+        content.localScale = new Vector3(scaleSlider.value, scaleSlider.value, scaleSlider.value);
+    }
+}
